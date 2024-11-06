fa524a03bed349c59022590ce830c79a6302fa80
VoxySc <TarikJarovicSc@gmail.com>
2024-11-06 22:54:17 +0100

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,16 @@
+function myf() {
+    window.open("https://youtube.com/@Voxy72"); 
+    alert("Hi from Dragan")
+    }
+    
+function mys() { 
+    window.open("https://github.com/Voxy72");
+     alert("Zuri8")
+    }
+function donke() {
+    SVGAnimatedAngle    
+}
+var SVGAnimatedAngle: {
+    new (): SVGAnimatedAngle;
+    prototype: SVGAnimatedAngle;
+}
