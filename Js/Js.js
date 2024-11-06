3cb4bc679f15916a77c6cd7f13b0c8ea54a6e117
VoxySc <TarikJarovicSc@gmail.com>
2024-11-06 23:42:50 +0100

    Add files via upload

---
+++ Js/Js.js
@@ -0,0 +1,21 @@
+function Home() {
+    window.location.href = 'index.html';
+}
+function Login() {
+    window.location.href = 'Login.html'
+}
+function Sign_Up() {
+    window.location.href = 'Sign_Up.html'
+}
+function myf() {
+    window.open("https://youtube.com/@Voxy72"); 
+    alert("Hi from Dragan")
+    }
+    
+function mys() { 
+    window.open("https://github.com/Voxy72");
+     alert("Zuri8")
+    }
+function donke() {
+    window.open('Zurio8.html', '_blank');
+    }
